03c6ea47bb395366dd3fa4548940cb163a1ff2ce
Shal Dengeki <shaldengeki@gmail.com>
2011-03-04 04:43:42 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Animurecs::Application.config.secret_token = 'e55512c548aaa2f02eb581bedb806dd2a137e0778620136049d2ab758f74f42da0da958b68d9cb47296b33e40bbd2edad2148d7e9e1a6749c6c040205b713853'
