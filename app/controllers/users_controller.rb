8964922013f7c079408422116eb87f7e9e21f624
Shal Dengeki <shaldengeki@gmail.com>
2011-03-10 22:14:16 +0300

    Completed basic signup work, moving to session auth next.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,6 +48,7 @@ class UsersController < ApplicationController
         format.html { redirect_to(@user, :notice => 'User was successfully created.') }
         format.xml  { render :xml => @user, :status => :created, :location => @user }
       else
+		@title = "Sign up"
         format.html { render :action => "new" }
         format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
       end
