7bd87ee9c7c6532fb3b48e64e9fd6a813d1fc85a
shaldengeki <shaldengeki@gmail.com>
2012-07-04 12:54:18 -0500

    fix asset pipeline paths.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,21 +1,21 @@
 module UsersHelper
 	def avatar_image(user = @user)
 		if user.username.nil? or user.avatar.nil?
-			image_tag("images/avatar-blank.png", :alt => "", :class => 'avatar')
+			image_tag("/assets/avatar-blank.png", :alt => "", :class => 'avatar')
 		else
 			image_tag user.avatar.url(:medium), :alt => "#{user.username}", :class => 'avatar'
 		end
 	end
 	def avatar_image_thumb(user = @user)
 		if user.username.nil? or user.avatar.nil?
-			image_tag("images/avatar-blank.png", :alt => "", :class => 'avatar_thumb')
+			image_tag("/assets/avatar-blank.png", :alt => "", :class => 'avatar_thumb')
 		else
 			image_tag user.avatar.url(:thumb), :alt => "#{user.username}", :class => 'avatar_thumb'
 		end
 	end
 	def user_header_profile_pic(user = @current_user)
 		if user.nil? or user.username.nil? or user.avatar.nil?
-			image_tag("images/avatar-blank.png", :alt => "", :class => 'avatar_thumb')
+			image_tag("/assets/avatar-blank.png", :alt => "", :class => 'avatar_thumb')
 		else
 			image_tag user.avatar.url(:tiny), :alt => "#{user.username}", :class => 'user_header_profile_pic'
 		end
