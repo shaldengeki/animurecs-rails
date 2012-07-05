723a17ef43e8f5515b0cdc723ffdf0b20dc38220
shaldengeki <shaldengeki@gmail.com>
2012-07-05 17:20:21 -0500

    Implement userrole management.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -73,14 +73,40 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     params[:user].delete(:id)
-    params[:user].delete(:role_ids)
+    if params[:user][:password].blank? or params[:user][:password_confirmation].blank?
+      sign_in_user = true
+    else
+      sign_in_user = false
+    end
     params[:user].delete(:password) if params[:user][:password].blank?
     params[:user].delete(:password_confirmation) if params[:user][:password_confirmation].blank?
     params[:user].delete(:avatar) if params[:user][:avatar].blank?
-
+    if can? :update, Userrole
+      params[:foo] = params[:user][:userrole_ids]
+      params[:user][:userrole_ids].each do |new_userrole_id|
+        unless new_userrole_id.blank?
+          new_userrole = Userrole.find(new_userrole_id)
+          @user.userroles << new_userrole unless @user.userroles.include?(new_userrole)
+        end
+      end
+      @user.save
+      params[:user].delete(:userrole_ids)
+    end
     respond_to do |format|
       if @user.update_attributes(params[:user])
-        format.html { redirect_to(@user, :notice => 'Your information has been updated. You will need to log in again.') }
+        pronoun = ' ' + @user.username
+        pronoun_possessive = @user.username + "'s'"
+        redirect_notice = ' will need to log in again.'
+        if current_user?(@user)
+          pronoun = ' You'
+          pronoun_possessive = "Your"
+        end
+        if sign_in_user
+          sign_in @user if current_user?(@user)
+          pronoun = ''
+          redirect_notice = ''
+        end
+        format.html { redirect_to(@user, :notice => pronoun_possessive + ' information has been updated.' + pronoun + redirect_notice) }
         format.xml  { head :ok }
       else
         format.html { render :action => "edit" }
